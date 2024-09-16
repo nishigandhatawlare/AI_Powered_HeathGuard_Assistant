e7aac9a959e829e29c72c598de46374c45368a23
“Nishigandha <nishigandhatawlare@gmail.com>
2024-09-16 15:09:57 +0530

    Implemented .NET Identity with custom ApplicationUser class for extended identity management.

---
+++ Health_Guard_Assistant.services.AuthService/Data/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Health_Guard_Assistant.services.AuthService.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Health_Guard_Assistant.services.AuthService.Data
+{
+
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
+
---
+++ Health_Guard_Assistant.services.AuthService/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Health_Guard_Assistant.services.AuthService.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public int Name { get; set; }
+    }
+}
